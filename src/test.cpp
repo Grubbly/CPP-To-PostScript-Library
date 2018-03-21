fd39578f213a0a7622c53c96f4acfa4b6665bea7
UAF Calamine <uafcalamine@gmail.com>
2018-03-21 00:32:45 -0800

    Implements catch framework with basic test case

---
+++ src/test.cpp
@@ -0,0 +1,28 @@
+/*
+test.cpp
+Tristan Van Cise & Collin Lasley
+Project 2 - CPS Library
+03/21/2018
+All test cases involving catch are defined here
+*/
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <iostream>
+using std::cin;
+
+int factorial(int a)
+{
+	return a <= 1 ? a : a * factorial(a - 1);
+}
+
+TEST_CASE("Catch working using factorial function?", "[Test Catch]")
+{
+	REQUIRE(factorial(2) == 2);
+	REQUIRE(factorial(3) == 6);
+}
+
+TEST_CASE("Shape", "[]")
+{
+	//add test cases here once classes are defined
+}
