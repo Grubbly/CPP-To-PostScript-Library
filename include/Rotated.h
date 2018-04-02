36afa1749a217d58939f1d9f99b99a2a2b11d7b9
cjlasley <cjlasley@alaska.edu>
2018-04-01 23:36:31 -0800

    Added empty-ish files for Rotated shapes

---
+++ include/Rotated.h
@@ -0,0 +1,13 @@
+//
+//  Rotated.h
+//  PostScriptXcode
+//
+//  Created by Collin Lasley on 4/1/18.
+//  Copyright © 2018 clasley. All rights reserved.
+//
+
+#ifndef Rotated_h
+#define Rotated_h
+
+
+#endif /* Rotated_h */
---
+++ src/Rotated.cpp
@@ -0,0 +1,9 @@
+//
+//  Rotated.cpp
+//  PostScriptXcode
+//
+//  Created by Collin Lasley on 4/1/18.
+//  Copyright © 2018 clasley. All rights reserved.
+//
+
+#include <stdio.h>
