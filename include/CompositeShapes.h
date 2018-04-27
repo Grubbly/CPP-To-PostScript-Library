0c5d223764886a251b3ffdb09d1d2f1dd8bcea93
cjlasley <cjlasley@alaska.edu>
2018-04-26 22:56:02 -0800

    Added empty files for CompositeShape base class
    The files do not implement any functions or functionality at all, yet.

---
+++ include/CompositeShapes.h
@@ -0,0 +1,13 @@
+//
+//  CompositeShapes.h
+//  CS372HW5
+//
+//  Created by Collin Lasley on 4/26/18.
+//  Copyright © 2018 clasley. All rights reserved.
+//
+
+#ifndef CompositeShapes_h
+#define CompositeShapes_h
+
+
+#endif /* CompositeShapes_h */
---
+++ src/CompositeShapes.cpp
@@ -0,0 +1,9 @@
+//
+//  CompositeShapes.cpp
+//  CS372HW5
+//
+//  Created by Collin Lasley on 4/26/18.
+//  Copyright © 2018 clasley. All rights reserved.
+//
+
+#include <stdio.h>
