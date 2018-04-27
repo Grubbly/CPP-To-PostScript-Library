3c23559ff82fa798956fe7c7844935c756ee9ba1
cjlasley <cjlasley@alaska.edu>
2018-04-26 23:16:18 -0800

    Added class declaration for composite shape
    The implementation is not yet used in any other classes

--- include/CompositeShapes.h
+++ include/CompositeShapes.h
@@ -10,5 +10,25 @@
 #ifndef COMPOSITESHAPES_h
 #define COMPOSITESHAPES_h
 
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "Shape.h"
+
+namespace PostLib {
+    class CompositeShape : public PostLib::Shape
+    {
+    public:
+        CompositeShape();
+        CompositeShape(const PostLib::PostScriptPoint &centerPoint, const std::initializer_list<std::unique_ptr<Shape>> &shapeList);
+        
+        virtual std::string PostScriptRepresentation(void) override final;
+                std::string postScript(void) final override;
+        
+    protected:
+        std::initializer_list<std::unique_ptr<Shape>> _shapeList;
+    };
+}
 
 #endif /* COMPOSITESHAPES_h */
